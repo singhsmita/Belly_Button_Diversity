b0e9ed4a868c28ff4a88fcb9e731ffc586c895af
Smita Singh <smita.rsingh09@gmail.com>
2023-03-20 02:37:02 -0400

    adding challenge assignment

--- charts.js
+++ charts.js
@@ -37,7 +37,6 @@ function buildMetadata(sample) {
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
-   
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
